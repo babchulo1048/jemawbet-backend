83b7d503d5fa7a1da9edb312817bd186745213e0
babchulo1048 <eyoeldemis1048@gmail.com>
2024-05-02 13:23:16 +0300

    refactor code on mongodb connection

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("mongodb connected"))
   .catch((ex) => console.log("Database connection error", ex.message));
 
